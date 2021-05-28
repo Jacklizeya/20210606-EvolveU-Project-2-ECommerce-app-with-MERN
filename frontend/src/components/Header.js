56ccae5f59220609d46fa31996d2da7f6d2410de
Chris Kelly <SCKelly@telus.net>
2021-05-27 20:39:42 -0600

    move search box

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -22,66 +22,65 @@ const Header = () => {
 
     // attempt bug fix for video 50
 
-    return (
-        <header>
-            <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand>
+
+return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <LinkContainer to='/'>
+                    <Navbar.Brand>
                             <img src='/images/3Dlogo5.png' alt='Image' height='100' width='100' className='ml-sm-3'></img>
-                        </Navbar.Brand>
-                    </LinkContainer>
-
-                    <LinkContainer to="/">
-                        <Navbar.Brand>
-                            <strong><h4>3D Store</h4></strong>
-                        </Navbar.Brand>
-                    </LinkContainer>
-
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar id="basic-navbar-nav">
-
-                        <Route render={
-                            ({ history }) => (<SearchBox history={history} />)} />
-
-
-                        <Nav className="mr-auto">
-
-                            <LinkContainer to="/cart">
-                                <Nav.Link>  <i className="fas fa-shopping-cart"> </i> Cart </Nav.Link>
+                    </Navbar.Brand>                
+                </LinkContainer>
+                <LinkContainer to='/'>
+                    <Navbar.Brand>
+                        <Navbar.Brand><strong><center><h4>3D Store</h4></center></strong></Navbar.Brand>   
+                    </Navbar.Brand>                
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Route render={({ history }) => <SearchBox history={history} />}/>
+                    <Nav className="ms-auto">
+                        <LinkContainer to='/cart'>
+                            <Nav.Link>
+                                <i className='fas fa-shopping-cart'></i> Cart
+                        </Nav.Link>
+                        </LinkContainer>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <i className='fas fa-user'></i> Sign In
+                            </Nav.Link>
                             </LinkContainer>
-
-                            {userInfo ?
-                                <NavDropdown title={userInfo.name}>
-                                    <LinkContainer to="/profile">
-                                        <NavDropdown.Item> Profile </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <NavDropdown.Item onClick={logoutHandler}> Logout </NavDropdown.Item>
-                                </NavDropdown> :
-                                (<LinkContainer to="/login">
-                                    <Nav.Link > <i className="fas fa-user"> </i>Sign In</Nav.Link>
-                                </LinkContainer>)}
-                            {userInfo && userInfo.isAdmin && (
-                                <NavDropdown title="Admin" id="adminmenu">
-                                    <LinkContainer to="/admin/userlist">
-                                        <NavDropdown.Item> Users </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <LinkContainer to="/admin/productlist">
-                                        <NavDropdown.Item> Products </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <LinkContainer to="/admin/orderlist">
-                                        <NavDropdown.Item> Orders </NavDropdown.Item>
-                                    </LinkContainer>
-                                </NavDropdown>)
-                            }
-                        </Nav>
-                    </Navbar>
-                </Container>
-            </Navbar>
-        </header>
-
-
-    )
+                        )}
+                        {userInfo && userInfo.isAdmin && (
+                            <NavDropdown title='Admin' id='adminmenu'>
+                                <LinkContainer to='/admin/userlist'>
+                                    <NavDropdown.Item>Users</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/productlist'>
+                                    <NavDropdown.Item>Products</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to='/admin/orderlist'>
+                                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+)
 }
 
 export default Header
