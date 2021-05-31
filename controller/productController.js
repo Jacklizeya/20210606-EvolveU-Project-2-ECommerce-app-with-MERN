264dcae58462465b5fb1aa2622d43eb7c1231186
Jacklizeya <jacklizeya@gmail.com>
2021-05-31 11:44:44 -0600

    fixrating

--- controller/productController.js
+++ controller/productController.js
@@ -103,7 +103,7 @@ const updateProduct = asyncHandler (async function (req, res, next){
 
       product.reviews.push(review)
       product.numReviews = product.reviews.length
-      product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+      product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
       await product.save()
       res.status(201).json({message: "review added"})
     } else {res.status(404); throw new Error("product no found")}
