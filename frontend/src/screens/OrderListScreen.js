6723be91608482283dfa5781774fa957768e5e36
Chris Kelly <SCKelly@telus.net>
2021-05-31 20:07:57 -0600

    GUP [Grammar Usage Punctuation] X-black

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -40,7 +40,7 @@ export default function OrderListScreen({history}) {
                         <th> ID </th>
                         <th> USER </th>
                         <th> DATE </th>
-                        <th> TOTAL Price </th>
+                        <th> TOTAL PRICE </th>
                         <th> PAID </th>
                         <th> DELIVERED </th>
                         <th>  </th>
@@ -54,8 +54,8 @@ export default function OrderListScreen({history}) {
                         <td> {order.user && order.user.name} </td>
                         <td> {order.createdAt.substring(0, 10)} </td>
                         <td> ${order.totalPrice} </td>
-                        <td> {order.isPaid? (order.paidAt.substring(0, 10)):(<i className = "fas fa-times" style={{color: "red"}}></i>)} </td>
-                        <td> {order.isDelivered? (order.deliveredAt.substring(0, 10)):(<i className = "fas fa-times" style={{color: "red"}}></i>)} </td>    
+                        <td> {order.isPaid? (order.paidAt.substring(0, 10)):(<i className = "fas fa-times" style={{color: "black"}}></i>)} </td>
+                        <td> {order.isDelivered? (order.deliveredAt.substring(0, 10)):(<i className = "fas fa-times" style={{color: "black"}}></i>)} </td>    
                         <td>
                             <LinkContainer to={`/order/${order._id}`}> 
                             <Button variant="light" className="btn-sm"> Details </Button>
